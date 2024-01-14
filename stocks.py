3d4add7a294f618acdff93704ae73bf4adb7ebfd
ferdousk <ferdousarnab@live.com>
2024-01-14 17:02:26 +1100

    test the dataframe
    print DF

--- stocks.py
+++ stocks.py
@@ -242,7 +242,7 @@ with base_info:
 		margin=dict(l=1,r=1,b=1,t=1),
 		#font=dict(color='#383635', size=15)
         )   
-    st.write(f"column names: {stock_stats['Attribute'].unique()}")
+    st.write(stock_stats)
     base_col_2.write(stock_perf_chart)
     
     
